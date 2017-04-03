1fee23d71f94c6d6a133d96122f8d7894c1c4669
Patrick Callier <pcallier@iqt.org>
2017-04-03 10:44:56 -0700

    Increase default number of FFT points

--- src/features/data_preprocessing.py
+++ src/features/data_preprocessing.py
@@ -13,7 +13,7 @@ from python_speech_features.sigproc import preemphasis
 def make_stft_features(signal, sample_rate,
                        output_sample_rate=1e4,
                        window_size=0.05, overlap=0.025,
-                       preemphasis_coeff=0.95, fft_size=0.0512):
+                       preemphasis_coeff=0.95, fft_size=0.1024):
     '''
     Function to take in a signal, resample it to output_sample_rate,
     normalize it, and compute the magnitude spectrogram.
@@ -52,7 +52,7 @@ def make_stft_dataset(data_dir, key_level, file_type, output_file,
                       output_sample_rate=1e4,
                       window_size=0.05, overlap=0.025,
                       preemphasis_coeff=0.95,
-                      track=None, fft_size=0.0512):
+                      track=None, fft_size=0.1024):
     '''
     Function to walk through a data directory data_dir and compute the stft
     features for each file of type file_type.  The computed features are
