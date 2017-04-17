a58c3fa5db487701d7c0e65f963fd606a9ea8402
Patrick Callier <pcallier@iqt.org>
2017-04-17 15:06:21 -0700

    Put PIT into package

---
+++ src/factorization/pit.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python
+import sys
+from itertools import islice, permutations, product
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+
+from ..features.mixer import FeatureMixer
+from ..features.wav_iterator import batcher
+from ..features.spectral_features import scale_spectrogram
+from ..utils.tf_bits import scope
+
+class PITModel:
+    def __init__(self, X_in, y_in, method='pit-s-cnn', num_srcs=2, num_steps=50, num_freq_bins=513):
+        self.X_in = X_in
+        self.y_in = y_in
+        self.num_steps = num_steps
+        self.num_freq_bins = num_freq_bins
+        self.num_srcs = num_srcs
+
+        if method=='pit-s-cnn':
+            self.network = self.cnn_mask
+        elif method=='pit-s-dnn':
+            self.network = self.dense_mask
+        elif method=='dense':
+            self.network = self.dense
+        else:
+            raise ValueError("Invalid network: {method}".format(method=method))
+
+        # Register tensor operations as object attributes
+        self.network
+        self.mask
+        self.predict
+        self.loss
+        self.optimize
+
+    @scope
+    def loss(self):
+        '''
+        Compute PIT loss across all examples in a minibatch,
+        respecting that each example in the minibatch might
+        have a different optimal mapping of truth to reconstruction
+
+        '''
+
+        # compute pairwise costs
+        # in 2-src case: either 1->2,2->1 or 1->1,2->2
+        losses = []
+        for src_id, out_id in product(range(self.num_srcs), range(self.num_srcs)):
+            loss = tf.reduce_mean(tf.squared_difference(self.predict[:, out_id],
+                                                        self.y_in[:, src_id]),
+                                 axis=(1, 2))
+            losses.append(loss)
+
+        # for each *permutation* of src->output assignments, look up the
+        # appropriate losses and sum
+        permuted_losses = []
+        for assignment in permutations(range(self.num_srcs), self.num_srcs):
+            permuted_loss = []
+            for src_id, out_id in enumerate(assignment):
+                loss_idx = src_id * self.num_srcs + out_id
+                permuted_loss.append(losses[loss_idx])
+            # sum losses over assigned pairings and add to list of permuted losses
+            permuted_loss = tf.stack(permuted_loss, axis=1)
+            print(permuted_loss)
+            permuted_loss = tf.reduce_sum(permuted_loss, axis=1)
+            print(permuted_loss)
+            permuted_losses.append(permuted_loss)
+
+        # select the minimum loss across assignment permutations
+        permuted_losses = tf.stack(permuted_losses,axis=1)
+        print(permuted_losses)
+        cost = tf.reduce_min(permuted_losses,axis=1)
+        print(cost)
+        cost = tf.reduce_sum(cost)
+        print(cost)
+        return cost
+
+    @scope
+    def optimize(self):
+        optimizer = tf.train.AdamOptimizer(0.001)
+        return optimizer.minimize(self.loss)
+
+    @scope
+    def predict(self):
+        return self.network[0]
+
+    @scope
+    def mask(self):
+        return self.network[1]
+
+    @scope
+    def dense(self):
+        '''
+        Home-brewed dense architecture, for testing. Regression-based (rather
+        than masking-based) approach
+        '''
+        data_shape = tf.shape(self.X_in)
+        # Reduce dimensionality
+        x = flatten(self.X_in)
+        x = tf.layers.dense(x, 1000)
+
+        # Split into two branches
+        y = tf.layers.dense(x, 500)
+        z = tf.layers.dense(x, 500)
+
+        # Reconstruct
+        y = tf.layers.dense(y, self.num_steps*self.num_freq_bins, None)
+        y = tf.reshape(y, data_shape)
+
+        z = tf.layers.dense(z, self.num_steps*self.num_freq_bins, None)
+        z = tf.reshape(z, data_shape)
+
+        return tf.stack((y,z), axis=1), None
+
+    @scope
+    def dense_mask(self):
+        '''
+        Replicates PIT-S-DNN architecture from Kolbaek et al. 2017
+        '''
+        x = flatten(self.X_in)
+        x = tf.layers.dense(x, 1024)
+        x = tf.layers.dense(x, 1024)
+        x = tf.layers.dense(x, 1024)
+        return self.mask_ops(x, x)
+
+    def mask_ops(self, y, z):
+        '''
+        Calculate and apply 2 masks from a dense transformation of the input
+        tensors. Uses softmax to ensure masks sum to one across src id.
+        '''
+        # Predict mask
+        y_mask = tf.layers.dense(y, self.num_steps*self.num_freq_bins, None)
+        z_mask = tf.layers.dense(z, self.num_steps*self.num_freq_bins, None)
+        all_masks = tf.stack((y_mask, z_mask), axis=0)
+        all_masks = tf.nn.softmax(all_masks, dim=0)
+        all_masks = tf.reshape(all_masks, (2, -1, self.num_steps, self.num_freq_bins))
+
+        # Reconstruct
+        reconstructions = all_masks * self.X_in
+        # Source,N,T,F -> N,Source,T,F
+        reconstructions = tf.transpose(reconstructions, [1, 0, 2, 3])
+
+        return reconstructions, all_masks
+
+    @scope
+    def cnn_mask(self):
+        '''
+        Attempts to replicate PIT-S-CNN architecture from Kolbaek et al. 2017
+        Parameter count is high and I have had trouble training it so far.
+        '''
+        x = tf.reshape(self.X_in, (-1, self.num_steps, self.num_freq_bins, 1))
+        x = tf.layers.conv2d(x, 64, kernel_size=(3, 3), strides=(2,2),
+                             name='conv1', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 64, kernel_size=(3, 3), strides=(1,1),
+                             name='conv2', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 64, kernel_size=(3, 3), strides=(1,1),
+                             name='conv3', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 64, kernel_size=(3, 3), strides=(1,1),
+                             name='conv4', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 64, kernel_size=(3, 3), strides=(1,1),
+                             name='conv5', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 128, kernel_size=(3, 3), strides=(2,2),
+                             name='conv6', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 128, kernel_size=(3, 3), strides=(1,1),
+                             name='conv7', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 128, kernel_size=(3, 3), strides=(1,1),
+                             name='conv8', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 256, kernel_size=(3, 3), strides=(2,2),
+                             name='conv9', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 256, kernel_size=(3, 3), strides=(1,1),
+                             name='conv10', padding='SAME', activation=tf.nn.relu)
+        x = tf.layers.conv2d(x, 256, kernel_size=(3, 3), strides=(1,1),
+                             name='conv11', padding='SAME', activation=tf.nn.relu)
+        x = tf.nn.max_pool(x, ksize=[1,3,3,1], strides=[1,3,3,1], padding='SAME', name='maxpool11')
+        x = flatten(x)
+        x = tf.layers.dense(x, 1024, activation=tf.nn.relu, name='dense12')
+
+        return self.mask_ops(x, x)
+
+    @scope
+    def blstm_mask(self):
+        raise NotImplementedError("Haven't finished BLSTM yet")
+        x = flatten(self.X_in)
+
+        fwd_lstm = tf.contrib.rnn.BasicLSTMCell(896)
+
+        x = tf.nn.bidirectional_dynamic_rnn()
+        return self.mask_ops(x, x)
+
+def training_setup(num_srcs, num_steps, num_freq_bins):
+    tf.reset_default_graph()
+
+    features = tf.placeholder(tf.float32, (None, num_steps, num_freq_bins))
+    references = tf.placeholder(tf.float32, (None, 2, num_steps, num_freq_bins))
+
+    model = PITModel(features, references, 'pit-s-dnn', num_srcs, num_steps, num_freq_bins)
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    return model, features, references, sess
+
+def training_loop(model, source_a, source_b, features, references, sess,
+                  num_steps=51, num_freq_bins=257, batch_size=128, num_batches=10000):
+    losses = []
+    num_srcs = 2
+    mixed_features = FeatureMixer([source_a, source_b], shape=(num_steps, None))
+    data_batches = batcher(mixed_features, batch_size)
+
+    for i, batch in enumerate(islice(data_batches, num_batches)):
+        # Unpack batch
+        batch_features, batch_ref1, batch_ref2 = batch
+        # Normalize
+        batch_ref1_norm, batch_ref1_norm_phase = scale_spectrogram(batch_ref1)
+        batch_ref2_norm, batch_ref2_norm_phase = scale_spectrogram(batch_ref2)
+        batch_features_norm, batch_features_norm_phase = scale_spectrogram(batch_features)
+        data = {
+            features: batch_features_norm,
+            references: np.stack((batch_ref1_norm, batch_ref2_norm), axis=1)
+            }
+        sess.run(model.optimize, feed_dict=data)
+        loss = sess.run(model.loss, feed_dict=data)
+        losses.append(loss)
+        prediction = sess.run(model.predict, data)
+
+    return losses
+
+if __name__=="__main__":
+    src1, src2 = sys.argv[1:]
+    model, features, references, sess = training_setup(2, 51, 257)
+    training_loop(model, src1, src2, features, references, sess, 51, 257, 64, 100)
+    saver = tf.train.Saver([model])
+    saver.save(sess, './pit_trained')
