572ddc9aab2af3272a42ba7c7879d94c07ca1469
Jeff Hetherly <jhetherly@iqt.org>
2017-08-29 22:28:25 +0000

    backwards compatability

--- src/features/preprocessing.py
+++ src/features/preprocessing.py
@@ -153,3 +153,68 @@ def make_stft_dataset(data_dir, file_type, output_file,
                                                  compression="gzip",
                                                  compression_opts=0)
         logger.info('looping over data finished')
+
+
+def make_stft_dataset_old(data_dir, key_level, file_type, output_file,
+                          output_sample_rate=1e4,
+                          window_size=0.0512, overlap=0.0256,
+                          preemphasis_coeff=0.95, fft_size=512,
+                          track=None):
+    '''DEPRECATED!
+    Function to walk through a data directory data_dir and compute the stft
+    features for each file of type file_type.  The computed features are
+    then stored in an hdf5 file with name output_file
+    Inputs:
+        data_dir: directory containing data (possibly in subdirs) (str)
+        key_level: Use folders at this depth as keys in the hdf5 file (int)
+        file_type: Extension of data files (Ex: '.wav') (str)
+        output_sample_rate: Sample rate to resample audio to (int)
+        window_size: Length of fft window in seconds (float)
+        overlap: Amount of window overlap in seconds (float)
+        preemphasis_coeff: preemphasis coefficient (float)
+        track: Track number to use for signals with multiple tracks (int)
+        fft_size: length (in seconds) of DFT window (float)
+    '''
+
+    # Open output file for writing
+    with h5py.File(output_file,'w') as data_file:
+
+        # Walk through data_dir and process all the files
+        for (dirpath, dirnames, filenames) in os.walk(data_dir, topdown=True):
+
+            # Get the key corresponding to the directory name at depth equal
+            # to key_level
+            depth = dirpath[len(data_dir):].count(os.path.sep)
+            if depth == key_level:
+                _, key = os.path.split(dirpath)
+
+                # Create a group for key if there isn't one already
+                if key not in data_file:
+                    data_file.create_group(key)
+
+            # Process any files in this directory
+            for file in filenames:
+                if os.path.splitext(file)[1] == file_type:
+                    file_path = os.path.join(dirpath,file)
+
+                    # Read in the signal and sample rate
+                    if track is not None:
+                        signal, sample_rate = sf.read(file_path)
+                        signal = signal[:,track]
+                    else:
+                        signal, sample_rate = sf.read(file_path)
+
+                    # Compute STFT spectrogram
+                    spectrogram = make_stft_features(signal,sample_rate,
+                                                     output_sample_rate,
+                                                     window_size,overlap,
+                                                     preemphasis_coeff,
+                                                     fft_size=fft_size)
+
+                    # Convert to 32 bit floats
+                    spectrogram = spectrogram.astype(np.complex64)
+
+                    data_file[key].create_dataset(os.path.splitext(file)[0],
+                                                  data=spectrogram,
+                                                  compression="gzip",
+                                                  compression_opts=0)
