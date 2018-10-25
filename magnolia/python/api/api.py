6bc5aea9d5dafbee3b1523160cedc0073e90821c
Ryan Ashley <rashley@iqt.org>
2018-10-25 14:44:12 -0400

    fully functional

--- magnolia/python/api/api.py
+++ magnolia/python/api/api.py
@@ -1,9 +1,11 @@
 import json
+import os
 import tempfile
 import logging.config
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 import librosa
+import numpy as np
 
 from magnolia.preprocessing.preprocessing import normalize_waveform
 from magnolia.preprocessing.preprocessing import preprocess_waveform
@@ -19,10 +21,22 @@ MODEL_DEVICE = '/cpu:0'
 ALLOWED_EXTENSIONS = set(['wav', 'mp3'])
 SAMPLING_RATE = 10000
 
+settings={
+    "compression": "gzip",
+    "compression_opts": 0,
+    "processing_parameters": {
+        "target_sample_rate": 10000,
+        "preemphasis_coeff": 0.0,
+        "stft_args": {
+            "n_fft": 512
+        }
+    }
+}
+
 app = Flask(__name__)
 CORS(app)
 app.config['UPLOAD_DIR'] = UPLOAD_DIR
-app.config['CONVERTED_DIR'] = UPLOAD_DIR
+app.config['CONVERTED_DIR'] = CONVERTED_DIR
 app.config['MODEL_DIR'] = MODEL_DIR
 
 def valid_file(filename):
@@ -41,23 +55,23 @@ def save_temp_file(request):
         return ''
     if file and valid_file(file.filename):
         tf = tempfile.NamedTemporaryFile()
-        filename = tf.name
+        filename = os.path.basename(tf.name)
         temp_path = os.path.join(app.config['UPLOAD_DIR'], filename)
         file.save(temp_path)
-        return file_path
+        return temp_path
 
 
 def convert_file_to_waveform(file_path):
     y,sr = librosa.load(file_path)
-    return y
+    duration = librosa.get_duration(y=y, sr=sr)
+    return y, duration
 
-def preprocess_waveform(wf_batch):
+def prepare_waveform(wf_batch):
     # Compute STFT spectrogram
     signal = normalize_waveform(np.squeeze(wf_batch))
-    D, yy = preprocess_waveform(signal, SAMPLING_RATE, **settings['processing_parameters'])
+    D,yy = preprocess_waveform(signal, SAMPLING_RATE, **settings['processing_parameters'])
     app.logger.debug(D.shape)
     app.logger.debug(yy.shape)
-
     return D
 
 def process_waveform(D):
@@ -80,8 +94,10 @@ def process_waveform(D):
     source_specs = chimera_mask(np.expand_dims(D,axis=0), model)[0]
     return source_specs
 
-def postprocess_waveform(source_specs):
-    yy_out = undo_preprocessing(source_specs[:,:,0], mixer.sample_length_in_bits(),
+def postprocess_waveform(source_specs, sample_length_in_bits):
+    istft_args={'hop_length': 256}
+    print(sample_length_in_bits)
+    yy_out = undo_preprocessing(source_specs[:,:,0], sample_length_in_bits,
                                    preemphasis_coeff=0,
                                    istft_args=istft_args)
 
@@ -89,10 +105,11 @@ def postprocess_waveform(source_specs):
 
 def convert_back_to_wav(yy):
     tf = tempfile.NamedTemporaryFile()
-    filename = tf.name
+    filename = os.path.basename(tf.name)
     temp_path = os.path.join(app.config['CONVERTED_DIR'], filename)
-    librosa.write_wav(temp_path, yy, SAMPLING_RATE)
-    return url_for('api/v1/converted/' + filename)
+    print(temp_path)
+    librosa.output.write_wav(temp_path, yy, SAMPLING_RATE)
+    return url_for('uploaded_file', filename=filename)
 
 @app.route('/api/v1/converted/<filename>', methods=['GET'])
 def uploaded_file(filename):
@@ -101,10 +118,11 @@ def uploaded_file(filename):
 @app.route('/api/v1/convert', methods=['POST'])
 def convert_file():
     temp_path = save_temp_file(request)
-    wf_batch = convert_file_to_waveform(temp_path)
-    D = preprocess_waveform(wf_batch)
+    wf_batch, len_in_secs = convert_file_to_waveform(temp_path)
+    len_in_bits = int(SAMPLING_RATE*len_in_secs)
+    D = prepare_waveform(wf_batch)
     source_specs = process_waveform(D)
-    yy = postprocess_waveform(source_specs)
+    yy = postprocess_waveform(source_specs, len_in_bits)
     converted_url = convert_back_to_wav(yy)
     return converted_url
 
